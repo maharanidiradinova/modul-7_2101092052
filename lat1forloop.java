3710bcbbbc7f1983684dcd6754ded6e0cdafb3df
maharanidiradinova <114593041+maharanidiradinova@users.noreply.github.com>
2022-11-18 01:51:47 +0700

    Add files via upload

---
+++ lat1forloop.java
@@ -0,0 +1,13 @@
+package Maharani2101092052_modul7;
+
+public class lat1forloop {
+  public static void main(String[]args)
+    {
+        String[] hari = { "Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"};
+        for(int i=0;i<hari.length;i++)
+            {
+                System.out.println(hari[i]);
+                ;
+            }
+    }  
+}
---
+++ lat2.java
@@ -0,0 +1,57 @@
+package Maharani2101092052_modul7;
+
+import javax.swing.JOptionPane;
+
+public class lat2 {
+
+    public static void main(String[] args) {
+
+        int[] a = new int[11];
+
+        String[] n = new String[11];
+
+        int terbesar = 0;
+
+        for (int i = 1; i + 1 <= a.length; i++) {
+
+            System.out.println(i); //menampilkan array ke
+
+            n[i] = JOptionPane.showInputDialog("Masukkan angka ke-" + i + " :");
+
+            a[i] = Integer.parseInt(n[i]);
+
+            if (terbesar > a[i]) {
+
+                if (terbesar > a[i]) {
+
+                    terbesar = terbesar;
+
+                    System.out.println(terbesar); //menampilkan terbesar dicommandline
+
+                }
+
+            } else {
+
+                if (a[i] > a[i - 1]) {
+
+                    terbesar = a[i];
+
+                } else {
+
+                    terbesar = a[i - 1];
+
+                }
+
+                System.out.println(terbesar); //menampilkan terbesar di commandline
+
+            }
+
+        }
+
+        String hasil = "Terbesar adalah " + terbesar;
+
+        JOptionPane.showMessageDialog(null, hasil);
+
+    }
+
+}
